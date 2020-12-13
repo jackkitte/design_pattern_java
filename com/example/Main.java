c5c557111bd9d50f0fe27c5aef5160f4469073fc
jackkitte <jackkitte735@gmail.com>
2020-12-13 09:19:59 +0900

    Singletonパターンの練習

--- com/example/Main.java
+++ com/example/Main.java
@@ -1,4 +1,18 @@
 package com.example;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start");
+
+        Singleton obj1 = Singleton.getInstance();
+        Singleton obj2 = Singleton.getInstance();
+
+        if (obj1 == obj2) {
+            System.out.println("2つは同じインスタンスです");
+        } else {
+            System.out.println("2つは異なるインスタンスです");
+        }
+
+        System.out.println("End");
+    }
 }
